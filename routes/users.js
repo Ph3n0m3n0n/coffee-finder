d67899790f411b39850772c73c84c4d1d06472fa
Kantaris <kantarisinnovations.staff@gmail.com>
2017-01-16 15:37:07 -0600

    added mongo to the mix... what a pain

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,116 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var session = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('users', { title: 'Jitz Map' });
+var User = require('../models/users');
+
+// Login
+router.get('/login', function(req, res){
+  var msg = req.session.msg;
+  req.session.msg = null;
+  console.log("the msg is: " + msg);
+  res.render('login', {msg: msg});
+});
+
+// Register
+router.get('/register', function(req, res){
+	res.render('register');
+});
+
+// Register User
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+  var message = req.body.message;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email:email,
+			username: username,
+			password: password,
+      message: message
+		});
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+      router.post('/adduser', function(req, res) {
+          var db = req.db;
+          var collection = db.get('userlist');
+          collection.insert(req.body, function(err, result){
+              res.send(
+                  (err === null) ? { msg: '' } : { msg: err }
+              );
+          });
+      });
+		});
+
+    req.session.msg = "Thank you for registering! You can login now!";
+    console.log("sucess on register");
+    res.redirect('/users/login');
+ }
+});
+
+// Local Passport Strategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   User.getUserByUsername(username, function(err, user){
+   	if(err) throw err;
+   	if(!user){
+   		return done(null, false, {message: 'Unknown User'});
+   	}
+
+   	User.comparePassword(password, user.password, function(err, isMatch){
+   		if(err) throw err;
+   		if(isMatch){
+   			return done(null, user);
+   		} else {
+   			return done(null, false, {message: 'Invalid password'});
+   		}
+   	});
+   });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login', passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login', failureFlash: true}), function(req, res) {
+    
+  console.log(req.body.username);
+  res.send('nothing');
+    // res.redirect('/');
+});
+
+router.get('/logout', function(req, res){
+	req.logout();
+	req.flash('success_msg', 'Goodbye!');
+	res.redirect('/users/login');
 });
 
 module.exports = router;
